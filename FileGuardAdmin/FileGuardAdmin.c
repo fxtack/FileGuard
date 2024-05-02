aee66b45cff56845f32b524b4c7bca86aad38ddf
fxtack <fxtack@qq.com>
2024-05-02 23:01:44 +0800

    test: support query rules

--- FileGuardAdmin/FileGuardAdmin.c
+++ FileGuardAdmin/FileGuardAdmin.c
@@ -18,15 +18,20 @@ int wmain(int argc, wchar_t* argv[]) {
     HANDLE port = INVALID_HANDLE_VALUE;
     FG_CORE_VERSION coreVersion = { 0 };
     FGL_RULE rules[] = {
-        { .RuleCode = RULE_READONLY, .RulePathExpression = L"*\\baned1.txt"},
-        { .RuleCode = RULE_READONLY, .RulePathExpression = L"*\\baned2.txt"},
-        { .RuleCode = RULE_READONLY, .RulePathExpression = L"*\\baned.txt"}
+        { RULE_READONLY, L"*\\baned1.txt" },
+        { RULE_READONLY, L"*\\baned2.txt" },
+        { RULE_READONLY, L"*\\baned.txt" }
     };
     FGL_RULE rule = { 0 };
     BOOLEAN added = FALSE, removed = FALSE;
     USHORT addedAmount = 0, removedAmount = 0;
     ULONG cleanRules = 0;
     PFG_MESSAGE message = NULL;
+
+    UCHAR *buffer = NULL;
+    USHORT queryAmount = 0;
+    ULONG querySize = 0ul;
+    FG_RULE *rulePtr = NULL;
 
     // Output
     WCHAR* filename = wcsrchr(argv[0], L'\\');
@@ -59,11 +64,11 @@ int wmain(int argc, wchar_t* argv[]) {
         goto Cleanup;
     }
 
-    hr = FglRemoveSingleRule(port, &rule, &removed);
-    if (FAILED(hr)) {
-        fprintf(stderr, "remove single rule failed: 0x%08x", hr);
-        goto Cleanup;
-    }
+    //hr = FglRemoveSingleRule(port, &rule, &removed);
+    //if (FAILED(hr)) {
+    //    fprintf(stderr, "remove single rule failed: 0x%08x", hr);
+    //    goto Cleanup;
+    //}
 
     hr = FglAddBulkRules(port, rules, 3, &addedAmount);
     if (FAILED(hr)) {
@@ -71,11 +76,35 @@ int wmain(int argc, wchar_t* argv[]) {
         goto Cleanup;
     }
 
-    hr = FglRemoveBulkRules(port, rules, 2, &removedAmount);
+    hr = FglQueryRules(port, NULL, 0, &queryAmount, &querySize);
+    printf("0x%08x, query result amount: %hu, size: %lu\n", hr, queryAmount, querySize);
+
+    buffer = malloc(querySize);
+    if (NULL == buffer) goto Cleanup;
+    hr = FglQueryRules(port, (FG_RULE*)buffer, querySize, &queryAmount, &querySize);
     if (FAILED(hr)) {
-        fprintf(stderr, "remove rules failed: 0x%08x", hr);
+        fprintf(stderr, "Query rules failed: 0x%08x", hr);
         goto Cleanup;
     }
+
+    rulePtr = buffer;
+    while (querySize > 0) {
+        printf("remain size: %lu, rule code: %lu, path expression: %.*ls, size: %lu\n", 
+               querySize,
+               rulePtr->RuleCode, 
+               rulePtr->PathExpressionSize/sizeof(WCHAR),
+               rulePtr->PathExpression,
+               rulePtr->PathExpressionSize);
+        rulePtr = ((UCHAR*)rulePtr) + (sizeof(FG_RULE) + rulePtr->PathExpressionSize);
+        querySize -= ((sizeof(FG_RULE) + rulePtr->PathExpressionSize));
+    }
+    
+
+    //hr = FglRemoveBulkRules(port, rules, 2, &removedAmount);
+    //if (FAILED(hr)) {
+    //    fprintf(stderr, "remove rules failed: 0x%08x", hr);
+    //    goto Cleanup;
+    //}
 
     //hr = FglCleanupRules(port, &cleanRules);
     //if (FAILED(hr)) {
