02e0e5e77f6aec9234f9fe29dc8cd2f587222b15
fxtack <fxtack@qq.com>
2024-05-06 22:35:56 +0800

    feat(WIP): admin cli show usage

--- FileGuardAdmin/FileGuardAdmin.cpp
+++ FileGuardAdmin/FileGuardAdmin.cpp
@@ -6,6 +6,8 @@
 #include <fltUser.h>
 
 #include <iostream>
+#include <sstream>
+#include <iomanip> 
 #include <string>
 #include <optional>
 #include <variant>
@@ -31,12 +33,8 @@ namespace fileguard {
             HANDLE port = INVALID_HANDLE_VALUE;
             HRESULT hr = FglConnectCore(&port);
             if (FAILED(hr)) return hr;
-            return std::make_unique<CoreClient>(port);
-        }
-
-        CoreClient(HANDLE port) {
-            port_.exchange(port);
-            GetCoreVersion();
+            std::unique_ptr<CoreClient> core_client(new CoreClient(port));
+            return std::move(core_client);
         }
 
         ~CoreClient() {
@@ -44,129 +42,177 @@ namespace fileguard {
             if (INVALID_HANDLE_VALUE != port) CloseHandle(port);
         }
 
-        HRESULT GetCoreVersion() {
+        std::variant<FG_CORE_VERSION, HRESULT> GetCoreVersion() {
             FG_CORE_VERSION core_version = {};
             auto hr = FglGetCoreVersion(port_, &core_version);
-            if (FAILED(hr)) return hr;
-            core_version_ = core_version;
+            if (SUCCEEDED(hr)) return core_version;
+            return hr;
         }
 
     private:
         std::optional<FG_CORE_VERSION> core_version_;
         std::atomic<HANDLE> port_;
+
+        CoreClient(HANDLE port) {
+            port_.exchange(port);
+        }
     };
 
     class Admin {
     public:
         static std::variant<std::unique_ptr<Admin>, HRESULT> NewAdmin(int argc, wchar_t** argv) noexcept {
-            auto admin = std::make_unique<Admin>(argc, argv);
+            auto core_client_opt = CoreClient::NewCoreClient();
+            std::unique_ptr<CoreClient> core_client = nullptr;
+            if (std::holds_alternative<std::unique_ptr<CoreClient>>(core_client_opt)) {
+                core_client = std::get<std::unique_ptr<CoreClient>>(std::move(core_client_opt));
+            }
+            
+            std::unique_ptr<Admin> admin(new Admin(argc, argv, std::move(core_client)));
             return std::move(admin);
         }
 
-        Admin(int argc, wchar_t** argv) {
-            argc_ = argc;
-            argv_ = argv;
-        };
         ~Admin() = default;
+
+        void Parse() {
+            PrintUsage();
+        }
 
         void PrintUsage() {
             const char* USAGE_FORMAT =
                 "%ws\n"
-                "Version: %hu.%hu.%hu.%hu\n"
+                "    Admin: v%hu.%hu.%hu.%hu\n"
+                "     Core: %s\n"
                 "This tool is used to operate rules\n"
                 "\n"
                 "Command\n"
-                "  add\n"
-                "    --expr, -e <expression> \n"
-                "    --type, -t <access-denied|readonly|hide>\n"
-                "  remove\n"
-                "    --expr, -e <expression>\n"
-                "    --type, -t <access-denied|readonly|hide>\n"
-                "  query\n"
-                "    --format, -f <list|csv|json>\n"
-                "  check-matched\n"
-                "    --path, -p <path>\n"
-                "  clean\n"
+                "    add\n"
+                "        --expr, -e <expression> \n"
+                "        --type, -t <access-denied|readonly|hide>\n"
+                "    remove\n"
+                "        --expr, -e <expression>\n"
+                "        --type, -t <access-denied|readonly|hide>\n"
+                "    query\n"
+                "        --format, -f <list|csv|json>\n"
+                "    check-matched\n"
+                "        --path, -p <path>\n"
+                "    clean\n"
                 "\n"
                 "Flags\n"
-                "  --help, -h\n"
-                "  -version, -v\n";
+                "    --help, -h\n"
+                "    --version, -v\n\n";
+            
+            // The core version is included in the usage information if it is successfully obtained.
+            std::string core_ver_str;
+            std::ostringstream oss;
+            if (nullptr != core_client_) {
+                auto core_ver_opt = core_client_->GetCoreVersion();
+                if (std::holds_alternative<FG_CORE_VERSION>(core_ver_opt)) {
+                    auto core_ver = std::get<FG_CORE_VERSION>(core_ver_opt);
+                    oss << "v"
+                        << core_ver.Major << "."
+                        << core_ver.Minor << "."
+                        << core_ver.Patch << "."
+                        << core_ver.Patch;
+                } else if (std::holds_alternative<HRESULT>(core_ver_opt)) {
+                    oss << "(error: 0x" << std::setfill('0') << std::setw(8)
+                        << std::hex << std::get<HRESULT>(core_ver_opt) 
+                        << ")";
+                }
+                core_ver_str = oss.str();
+            } else {
+                core_ver_str = "(core not connected)";
+            }
 
-            printf(USAGE_FORMAT, AdminImageName(),
-                FGA_MAJOR_VERSION, FGA_MINOR_VERSION,
-                FGA_PATCH_VERSION, FGA_BUILD_VERSION);
+            printf(USAGE_FORMAT, AdminImageName().c_str(),
+                   FGA_MAJOR_VERSION, FGA_MINOR_VERSION,
+                   FGA_PATCH_VERSION, FGA_BUILD_VERSION, core_ver_str.c_str());
         }
 
     private:
         int argc_;
         wchar_t** argv_;
+        std::unique_ptr<CoreClient> core_client_;
 
         std::wstring& AdminImageName() {
             static std::wstring image_name;
             if (image_name.empty()) {
                 std::wstring image_path = argv_[0];
-                size_t pos = image_path.find_first_of('\\');
+                size_t pos = image_path.find_last_of('\\');
                 image_name = image_path.substr(pos + 1);
             }
             return image_name;
         }
 
-        std::optional<std::unique_ptr<CoreClient>> core_client_;
+        Admin(int argc, wchar_t** argv, std::unique_ptr<CoreClient> core_client) {
+            argc_ = argc;
+            argv_ = argv;
+            core_client_ = std::move(core_client);
+        };
     };
 }
 
 int wmain(int argc, wchar_t* argv[]) {
-    HRESULT hr = S_OK;
-    HANDLE port = INVALID_HANDLE_VALUE;
-    FG_CORE_VERSION coreVersion = { 0 };
-    FGL_RULE rules[] = {
-        { RULE_READONLY, L"*\\baned.txt" },
-        { RULE_READONLY, L"*baned.txt" },
-        { RULE_READONLY, L"*\\baned.txt" }
-    };
-    FGL_RULE rule = { 0 };
-    BOOLEAN added = FALSE, removed = FALSE;
-    USHORT addedAmount = 0, removedAmount = 0;
-    ULONG cleanRules = 0;
-    PFG_MESSAGE message = NULL;
-
-    UCHAR *buffer = NULL;
-    USHORT queryAmount = 0;
-    ULONG querySize = 0ul;
-    FG_RULE *rulePtr = NULL;
-    ULONG thisRuleSize = 0ul;
-    INT i = 0;
-
-    // Output
-    WCHAR* filename = wcsrchr(argv[0], L'\\');
-    if (NULL == filename) {
-        filename = argv[0];
+    
+    auto admin_opt = fileguard::Admin::NewAdmin(argc, argv);
+    if (std::holds_alternative<HRESULT>(admin_opt)) {
+        auto hr = std::get<HRESULT>(admin_opt);
+        std::cerr << "0x" << std::hex << std::setfill('0') << std::setw(8) << hr << std::endl;
+        return hr;
     }
 
-    hr = FglConnectCore(&port);
-    if (FAILED(hr)) {
-        fprintf(stderr, "connect core failed: 0x%08x", hr);
-        goto Cleanup;
-    }
+    std::get<std::unique_ptr<fileguard::Admin>>(std::move(admin_opt))->Parse();
 
-    hr = FglGetCoreVersion(port, &coreVersion);
-    if (FAILED(hr)) {
-        fprintf(stderr, "get core version failed: 0x%08x", hr);
-        goto Cleanup;
-    }
+    //HRESULT hr = S_OK;
+    //HANDLE port = INVALID_HANDLE_VALUE;
+    //FG_CORE_VERSION coreVersion = { 0 };
+    //FGL_RULE rules[] = {
+    //    { RULE_READONLY, L"*\\baned.txt" },
+    //    { RULE_READONLY, L"*baned.txt" },
+    //    { RULE_READONLY, L"*\\baned.txt" }
+    //};
+    //FGL_RULE rule = { 0 };
+    //BOOLEAN added = FALSE, removed = FALSE;
+    //USHORT addedAmount = 0, removedAmount = 0;
+    //ULONG cleanRules = 0;
+    //PFG_MESSAGE message = NULL;
 
-    printf("%ws v%hu.%hu.%hu.%hu, core version: v%hu.%hu.%hu.%hu\n",
-           ++filename,
-           FGA_MAJOR_VERSION, FGA_MINOR_VERSION, FGA_PATCH_VERSION, FGA_BUILD_VERSION,
-           coreVersion.Major, coreVersion.Minor, coreVersion.Patch, coreVersion.Build);
+    //UCHAR *buffer = NULL;
+    //USHORT queryAmount = 0;
+    //ULONG querySize = 0ul;
+    //FG_RULE *rulePtr = NULL;
+    //ULONG thisRuleSize = 0ul;
+    //INT i = 0;
 
-    rule.RuleCode = RULE_READONLY;
-    rule.RulePathExpression = L"\\Device\\HarddiskVolume\\*";
-    hr = FglAddSingleRule(port, &rule, &added);
-    if (FAILED(hr)) {
-        fprintf(stderr, "add single rule failed: 0x%08x", hr);
-        goto Cleanup;
-    }
+    //// Output
+    //WCHAR* filename = wcsrchr(argv[0], L'\\');
+    //if (NULL == filename) {
+    //    filename = argv[0];
+    //}
+
+    //hr = FglConnectCore(&port);
+    //if (FAILED(hr)) {
+    //    fprintf(stderr, "connect core failed: 0x%08x", hr);
+    //    goto Cleanup;
+    //}
+
+    //hr = FglGetCoreVersion(port, &coreVersion);
+    //if (FAILED(hr)) {
+    //    fprintf(stderr, "get core version failed: 0x%08x", hr);
+    //    goto Cleanup;
+    //}
+
+    //printf("%ws v%hu.%hu.%hu.%hu, core version: v%hu.%hu.%hu.%hu\n",
+    //       ++filename,
+    //       FGA_MAJOR_VERSION, FGA_MINOR_VERSION, FGA_PATCH_VERSION, FGA_BUILD_VERSION,
+    //       coreVersion.Major, coreVersion.Minor, coreVersion.Patch, coreVersion.Build);
+
+    //rule.RuleCode = RULE_READONLY;
+    //rule.RulePathExpression = L"\\Device\\HarddiskVolume\\*";
+    //hr = FglAddSingleRule(port, &rule, &added);
+    //if (FAILED(hr)) {
+    //    fprintf(stderr, "add single rule failed: 0x%08x", hr);
+    //    goto Cleanup;
+    //}
 
     ////hr = FglRemoveSingleRule(port, &rule, &removed);
     ////if (FAILED(hr)) {
@@ -174,11 +220,11 @@ int wmain(int argc, wchar_t* argv[]) {
     ////    goto Cleanup;
     ////}
 
-    hr = FglAddBulkRules(port, rules, 3, &addedAmount);
-    if (FAILED(hr)) {
-        fprintf(stderr, "add rules failed: 0x%08x", hr);
-        goto Cleanup;
-    }
+    //hr = FglAddBulkRules(port, rules, 3, &addedAmount);
+    //if (FAILED(hr)) {
+    //    fprintf(stderr, "add rules failed: 0x%08x", hr);
+    //    goto Cleanup;
+    //}
 
     //hr = FglQueryRules(port, NULL, 0, &queryAmount, &querySize);
     //printf("0x%08x, query result amount: %hu, size: %lu\n", hr, queryAmount, querySize);
@@ -218,42 +264,42 @@ int wmain(int argc, wchar_t* argv[]) {
     //    printf("Cleanup %lu rules\n", cleanRules);
     //}
     
-    hr = FglCheckMatchedRules(port,
-                              L"\\Device\\HarddiskVolume6\\baned.txt",
-                              NULL, 
-                              0,
-                              &queryAmount,
-                              &querySize);
-    printf("hresult: 0x%08x, matched amount: %hu, size: %lu\n", hr, queryAmount, querySize);
-
-    buffer = (UCHAR*)malloc(querySize);
-    if (NULL == buffer) goto Cleanup;
-    else RtlZeroMemory(buffer, querySize);
-    hr = FglCheckMatchedRules(port,
-                             L"\\Device\\HarddiskVolume6\\baned.txt",
-                             (FG_RULE*)buffer, 
-                             querySize,
-                             &queryAmount, 
-                             &querySize);
-    printf("hresult: 0x%08x, matched amount: %hu, size: %lu\n", hr, queryAmount, querySize);
-    rulePtr = (FG_RULE*)buffer;
-    while (querySize > 0) {
-        printf("remain size: %lu, rule code: %lu, path expression: %.*ls, size: %lu\n", 
-               querySize,
-               rulePtr->RuleCode, 
-               rulePtr->PathExpressionSize/sizeof(WCHAR),
-               rulePtr->PathExpression,
-               rulePtr->PathExpressionSize);
-
-        thisRuleSize = sizeof(FG_RULE) + rulePtr->PathExpressionSize;
-        printf("This rule size: %lu\n", thisRuleSize);
-        rulePtr = (FG_RULE*)Add2Ptr(rulePtr, thisRuleSize);
-        querySize -= thisRuleSize;
-    }
-
-Cleanup:
-
-    if (INVALID_HANDLE_VALUE != port) {
-        FglDisconnectCore(port);
-    }
+//    hr = FglCheckMatchedRules(port,
+//                              L"\\Device\\HarddiskVolume6\\baned.txt",
+//                              NULL, 
+//                              0,
+//                              &queryAmount,
+//                              &querySize);
+//    printf("hresult: 0x%08x, matched amount: %hu, size: %lu\n", hr, queryAmount, querySize);
+//
+//    buffer = (UCHAR*)malloc(querySize);
+//    if (NULL == buffer) goto Cleanup;
+//    else RtlZeroMemory(buffer, querySize);
+//    hr = FglCheckMatchedRules(port,
+//                             L"\\Device\\HarddiskVolume6\\baned.txt",
+//                             (FG_RULE*)buffer, 
+//                             querySize,
+//                             &queryAmount, 
+//                             &querySize);
+//    printf("hresult: 0x%08x, matched amount: %hu, size: %lu\n", hr, queryAmount, querySize);
+//    rulePtr = (FG_RULE*)buffer;
+//    while (querySize > 0) {
+//        printf("remain size: %lu, rule code: %lu, path expression: %.*ls, size: %lu\n", 
+//               querySize,
+//               rulePtr->RuleCode, 
+//               rulePtr->PathExpressionSize/sizeof(WCHAR),
+//               rulePtr->PathExpression,
+//               rulePtr->PathExpressionSize);
+//
+//        thisRuleSize = sizeof(FG_RULE) + rulePtr->PathExpressionSize;
+//        printf("This rule size: %lu\n", thisRuleSize);
+//        rulePtr = (FG_RULE*)Add2Ptr(rulePtr, thisRuleSize);
+//        querySize -= thisRuleSize;
+//    }
+//
+//Cleanup:
+//
+//    if (INVALID_HANDLE_VALUE != port) {
+//        FglDisconnectCore(port);
+//    }
 }
